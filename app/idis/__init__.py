a68e354d3fb04cd82b8a641cdc909a7ad0bdc84e
sjoerdk <sjoerd.kerkstra@radboudumc.nl>
2018-12-21 10:57:32 +0100

    Bump version: 0.1.16 → 0.1.17

--- app/idis/__init__.py
+++ app/idis/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "__version__ = '__version__ = '0.1.16''"
+__version__ = "__version__ = '__version__ = '0.1.17''"
 __version_info__ = tuple(
     [
         int(num) if num.isdigit() else num

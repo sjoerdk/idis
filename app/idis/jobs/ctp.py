d79ffb3aa88d883edf246a19b38135e89a01cfdf
sjoerdk <sjoerd.kerkstra@radboudumc.nl>
2019-02-20 11:52:20 +0100

    Adds CTP quarantine handling TRAC:#8155

---
+++ app/idis/jobs/ctp.py
@@ -0,0 +1,433 @@
+""" Functions and classes for dealing with CTP. CTP is the java-based program that does the actual anonymization.
+
+"""
+from pathlib import Path
+from typing import List
+
+import pydicom
+
+from idis.jobs.filehandling import JobFolder, JobFile, move_job_data, move_job_file
+from pydicom.datadict import add_private_dict_entries
+from pydicom.errors import InvalidDicomError
+
+
+class CTPQuarantineFolder:
+    """A quarantine made by CTP for a single stage"""
+
+    def __init__(self, path, description=None):
+        """
+
+        Parameters
+        ----------
+        path: Path
+            full path to this quarantine folder
+        description: str, optional
+            description of this folder. Used for telling users why file might end up in this folder. Defaults to folder
+            name
+        """
+        self.path = Path(path)
+        if description:
+            self.description = f"Quarantine folder {self.path.name} ({description})"
+        else:
+            self.description = f"Quarantine folder {self.path.name}"
+
+    def __str__(self):
+        return f"CTP quarantine folder at {self.path}"
+
+    def get_files(self):
+        """Get paths to all files in this quarantine folder
+
+        Returns
+        -------
+        List[Path]
+            full path to each file in this Quarantine folder
+
+        """
+        return list(x for x in self.path.glob("*") if x.is_file())
+
+    def get_job_files(self):
+        """
+
+        Returns
+        -------
+        List[JobFile]
+            job for each file in this folder, or None if no job can be found
+
+        """
+
+        paths = self.get_files()
+        job_files = []
+        for path in paths:
+            job_files.append(self.path_to_job_file(path))
+
+        return job_files
+
+    @staticmethod
+    def path_to_job_file(path):
+        """Return JobFile with associated job for each path
+
+        Parameters
+        ----------
+        path: Path
+            path to a DICOM file that has IDIS private tag for JobID set
+
+        Returns
+        -------
+        JobFile
+            containing file path and associated job. If file cannot be associated with job JobFile.job_id will be None
+
+        Notes
+        -----
+        returned JobFile.job_id can be None if file cannot be associated with a job
+
+
+        """
+        try:
+            ds = IDISDICOMDataSet(pydicom.dcmread(str(path)))
+        except InvalidDicomError:
+            return JobFile(job_id=None, path=path)
+        try:
+            job_id = ds.get_idis_tag_value("JobID")
+            return JobFile(job_id=job_id, path=path)
+        except KeyError:
+            return JobFile(job_id=None, path=path)
+
+
+class IDISDICOMDataSet:
+    """Holds a pydicom dataset, adds extra functionality for dealing with IDIS-specific private tags
+
+    Alternative to this would be to add to pydicom itself and post merge requests, but that is for later.
+    """
+
+    PRIVATE_CREATOR = "RADBOUDUMCANONYMIZER"
+    IDIS_PRIVATE_TAGS = {
+        0x00750027: ("LO", "1", "JobID"),
+        0x00750028: ("LO", "1", "SourceInstanceID", "", "SourceInstanceID"),
+    }
+
+    def __init__(self, dataset):
+        """
+
+        Parameters
+        ----------
+        dataset: pydicom.dataset.FileDataset
+            wrap around this dataset
+        """
+        self.dataset = dataset
+        add_private_dict_entries(self.PRIVATE_CREATOR, self.IDIS_PRIVATE_TAGS)
+
+    def get_idis_tag_value(self, tag_name):
+        """Get the dicom value given by the idis private tag tag_name
+
+        Parameters
+        ----------
+        tag_name: str
+            name of the tag
+
+        Raises
+        ------
+        KeyError:
+            when tag is not found in this dataset
+
+        """
+
+        private_tags = [de for de in self.dataset if hasattr(de, "private_creator")]
+        idis_tags = {
+            tag.name: tag
+            for tag in private_tags
+            if tag.private_creator == self.PRIVATE_CREATOR
+        }
+        # pydicom puts square brackets around the names of private tags to differentiate it from standard tags
+        return idis_tags[f"[{tag_name}]"].value
+
+
+class IDISQuarantineFolder(JobFolder):
+    """A quarantine folder in which each file is always associated with a job id"""
+
+    def __init__(self, path, description):
+        """
+
+        Parameters
+        ----------
+        path: Path or str
+            full path to this folder
+
+        description: str, optional
+            description of this folder. Used for telling users why file might end up in this folder. Defaults to folder
+            name
+
+        description
+        """
+        super().__init__(path)
+        self.description = description
+
+    def __str__(self):
+        return f"IDIS quarantine folder at {self.path}"
+
+    def get_files(self, job_id: int):
+        """ get paths to all files belonging to the given job, add source quarantine folder
+
+        Returns
+        -------
+        List[QuarantinedJobFile]
+            list with each file belonging to the given job
+
+        Raises
+        ------
+        FileNotFoundError
+            when given job id does not exist in this folder
+        """
+        job_files: JobFile = super().get_files(job_id)
+        return [self.to_quarantine_job_file(job_file) for job_file in job_files]
+
+    def to_quarantine_job_file(self, job_file: JobFile):
+        """Add link to this quarantine dir to given JobFile
+
+        """
+
+        return QuarantinedJobFile(
+            job_id=job_file.job_id, path=job_file.path, quarantine_folder=self
+        )
+
+
+class IDISCTPQuarantine:
+    """Scrapes CTP quarantine folders. Answers questions such as 'How many files are quarantined for job X'
+
+    Notes
+    -----
+    CTP has quarantine folders for each of its stages. It cannot be configured to separate files per
+    job. Hence this class that can scrape out all quarantined files and sort them properly.
+
+    IDISCTPQuarantine knows three main sets of folders:
+
+    * CTP quarantine folders:           Folders that CTP fills with quarantined files
+    * IDIS active quarantine folders:   Mirrors CTP folders. Base for most IDISCTPQuarantine functions
+    * IDIS archived quarantine folders: Files here are mostly ignored, saved for future reference
+
+    """
+
+    def __init__(self, base_folder: Path, ctp_quarantine_folders):
+        """Create an IDIS quarantine that scrapes the given CTP quarantine folders to base_folder and
+        makes their contents manageable.
+
+        Parameters
+        ----------
+        base_folder: Path
+            Keep all data in this folder
+
+        ctp_quarantine_folders: List[CTPQuarantineFolder]
+            list of CTP quarantine folders to mirror
+
+        """
+        self.base_folder = Path(base_folder)
+        self.active_base_folder = self.base_folder / "active"
+        self.archived_base_folder = self.base_folder / "archived"
+        self.ctp_folder_mapping = self.create_ctp_folder_mapping(ctp_quarantine_folders)
+        self.archive_mapping = self.create_archive_mapping(self.active_quarantine_folders)
+
+    def __str__(self):
+        return f"IDIS CTP quarantine at {self.base_folder}"
+
+    @property
+    def ctp_folders(self):
+        """All CTP folders that this Quarantine mirrors """
+        return list(self.ctp_folder_mapping.keys())
+
+    @property
+    def active_quarantine_folders(self):
+        """All active IDIS quarantine folders """
+        return list(self.ctp_folder_mapping.values())
+
+    def create_ctp_folder_mapping(
+        self, ctp_quarantine_folders: List[CTPQuarantineFolder]
+    ):
+        """Return a mapping from each CTP quarantine folder to the active IDIS quarantine folder that mirrors it
+
+        Parameters
+        ----------
+        ctp_quarantine_folders: List[QuarantineFolder]
+
+        Returns
+        -------
+        Dict[Path: IDISQuarantineFolder]
+            mapping from each ctp to idis quarantine folder
+
+        """
+        folder_links = {}
+        for ctp_folder in ctp_quarantine_folders:
+            ctp_folder_name = ctp_folder.path.name
+            idis_mirror_folder = IDISQuarantineFolder(
+                path=self.active_base_folder / ctp_folder_name,
+                description=ctp_folder.description,
+            )
+            folder_links[ctp_folder] = idis_mirror_folder
+        return folder_links
+
+    def scrape(self):
+        """Move all files from CTP quarantine to this folder
+
+        Notes
+        -----
+        Reads DICOM tags from each file to determine job id
+        """
+        for ctp_folder, idis_folder in self.ctp_folder_mapping.items():
+            ctp_folder: CTPQuarantineFolder
+            job_files = ctp_folder.get_job_files()
+            for job_file in job_files:
+                move_job_file(job_file, destination=idis_folder)
+
+    def archive(self, job_id):
+        """Move all files for this job from activate to archive
+
+        Parameters
+        ----------
+        job_id
+
+        """
+        for active, archive in self.archive_mapping.items():
+            move_job_data(job_id=job_id, source=active, destination=archive)
+
+    def get_files(self, job_id):
+        """Get all files belonging to the given job from this quarantine
+
+        Returns
+        -------
+        List[QuarantinedJobFile]
+            All files belonging to this job, linked to the quarantine they were found in
+
+        Raises
+        ------
+        FileNotFoundError
+            when given job id does not exist in this folder
+
+        """
+        files = []
+        for idis_q_folder in self.ctp_folder_mapping.values():
+            try:
+                files = files + idis_q_folder.get_files(job_id)
+            except FileNotFoundError:
+                continue
+        return files
+
+    def get_file_count(self, job_id):
+        """Get number of files that are in quarantine for this job
+        
+        Parameters
+        ----------
+        job_id: int
+            job id to check
+        
+        Returns
+        -------
+        int
+            number of files that are in this IDIS quarantine for this job
+
+        Notes
+        -----
+        Returns 0 if the job id is not in quarantine at all
+
+        """
+        count = 0
+        for idis_q_folder in self.ctp_folder_mapping.values():
+            try:
+                count += idis_q_folder.get_file_count(job_id)
+            except FileNotFoundError:
+                continue
+        return count
+
+    def get_unknown_job_files(self):
+        """Get all files in the quarantine not mapped to any job
+
+        Returns
+        -------
+        List of Path
+            All files belonging to this job, linked to the quarantine they were found in
+
+        Raises
+        ------
+        FileNotFoundError
+            when given job id does not exist in this folder
+
+        """
+        files = []
+        for idis_folder in self.ctp_folder_mapping.values():
+            try:
+                job_files = idis_folder.get_unknown_job_files()
+                files = files + [
+                    idis_folder.to_quarantine_job_file(x) for x in job_files
+                ]
+            except FileNotFoundError:
+                continue
+
+        return files
+
+    def get_job_ids(self):
+        """Get ids of all jobs for which there are files in this quarantine
+
+        Returns
+        -------
+        List of int
+            id for each job that has files in this quarantine
+
+        """
+        ids = []
+        for idis_folder in self.ctp_folder_mapping.values():
+            ids = ids + idis_folder.get_job_ids()
+        return list(set(ids))
+
+    def create_archive_mapping(self, active_quarantine_folders):
+        """Map each active quarantine folder to an archive mirror
+
+        Parameters
+        ----------
+        active_quarantine_folders: List[IDISQuarantineFolder]
+
+        Returns
+        -------
+        Dict[IDISQuarantineFolder, IDISQuarantineFolder]
+            mapping active folder -> archive folder for each input folder
+        """
+
+        mapping = {}
+        for active in active_quarantine_folders:
+            archived = IDISQuarantineFolder(path=self.archived_base_folder / active.path.name,
+                                            description="Archive for " + active.description)
+            mapping[active] = archived
+        return mapping
+
+
+class QuarantinedJobFile(JobFile):
+    """A JobFile linked to a certain quarantine directory"""
+
+    def __init__(self, *args, quarantine_folder: CTPQuarantineFolder, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.quarantine_folder = quarantine_folder
+
+
+class IDISServer:
+    """Collection of all locations and functions
+
+    """
+
+    def __init__(
+        self, pre_fetching_path, CTP_input_path, CTP_output_path, quarantine_base_path
+    ):
+        """
+
+        Parameters
+        ----------
+        pre_fetching_path: str
+            full path to folder to put newly downloaded files in
+        CTP_input_path: str
+            full path to the folder that CTP reads its input files from
+        CTP_output_path: str
+            full path to CTP output folder
+        quarantine_base_path: str
+            full path to quarantine directory
+        """
+
+        self.pre_fetching_folder = JobFolder(pre_fetching_path)
+        # self.CTP_server =
+
+class JobFileParseException(Exception):
+    pass
---
+++ app/tests/jobs_tests/test_ctp.py
@@ -0,0 +1,138 @@
+from distutils import dir_util
+from pathlib import Path
+
+import pytest
+
+from idis.jobs.ctp import CTPQuarantineFolder, IDISCTPQuarantine
+from tests.jobs_tests import RESOURCE_PATH
+
+
+@pytest.fixture
+def test_resources_folder(tmpdir):
+    """An example of a CTP quarantine base folder with several stages, some job files and some invalid files
+
+    Returns
+    -------
+    str
+        path to folder
+
+    """
+    template_folder = Path(RESOURCE_PATH) / "test_ctp" / "ctp_q"
+    dir_util.copy_tree(str(template_folder), str(tmpdir))
+    return tmpdir
+
+
+@pytest.fixture
+def empty_folder(tmpdir_factory):
+    """One-time empty folder
+
+    Returns
+    -------
+    str
+        path to folder
+
+    """
+    return tmpdir_factory.mktemp(basename="empty")
+
+
+@pytest.fixture()
+def idis_ctp_quarantine(test_resources_folder, empty_folder):
+    """ An idis quarantine folder linked to some test ctp folders with some files"""
+
+    q_folders = [
+        CTPQuarantineFolder(
+            test_resources_folder / "DicomAnonymizerFullDates",
+            description="Something went wrong with settings dates",
+        ),
+        CTPQuarantineFolder(
+            test_resources_folder / "DicomAnonymizerKeepSafePrivateTags",
+            description="Some unsupported private tags might be in your data",
+        ),
+        CTPQuarantineFolder(test_resources_folder / "DicomAnonymizerModifiedDates"),
+        CTPQuarantineFolder(
+            test_resources_folder / "DicomAnonymizerFaultyFiles",
+            description="Things went really wrong",
+        ),
+    ]
+    return IDISCTPQuarantine(
+        base_folder=Path(empty_folder), ctp_quarantine_folders=q_folders
+    )
+
+
+def test_ctp_quarantine(test_resources_folder):
+    """Check getting all files from quarantine folder"""
+    qf = CTPQuarantineFolder(test_resources_folder / "DicomAnonymizerFullDates")
+    files = qf.get_files()
+    assert [x.name for x in files] == ["file1", "file2"]
+
+
+def test_ctp_quarantine_job_files(test_resources_folder):
+    """Check getting all files from quarantine folder, but also figure out which job they belong to"""
+    qf = CTPQuarantineFolder(test_resources_folder / "DicomAnonymizerFullDates")
+    job_files = qf.get_job_files()
+    assert [x.name for x in job_files] == ["file1", "file2"]
+    assert [x.job_id for x in job_files] == [1, 2]
+
+
+@pytest.mark.parametrize(
+    "file_name", ["file8_no_dicom", "file7_no_private_creator", "file6_no_job_id"]
+)
+def test_idis_dicom_dataset_messy_input(test_resources_folder, file_name):
+    """Ensuring responses for messy files. Should return JobFile with None for job id"""
+    qf = CTPQuarantineFolder(test_resources_folder / "DicomAnonymizerFaultyFiles")
+    files = {file.name: file for file in qf.get_files()}
+    job_file = qf.path_to_job_file(files[file_name])
+    assert job_file.job_id is None
+
+
+def test_ctp_quarantine_job_files_messy_input(test_resources_folder):
+    """Try to sort quarantine folder with files that have problems.. missing tags, not dicom files etc."""
+    qf = CTPQuarantineFolder(test_resources_folder / "DicomAnonymizerFaultyFiles")
+
+    # folder with 1 OK, 3 faulty files should not cause exceptions but instead yield some jobs with job_id = None
+    job_files = qf.get_job_files()
+    assert len(job_files) == 4
+    assert [x.job_id for x in job_files] == [1, None, None, None]
+
+
+def test_idis_ctp_quarantine_scraping(idis_ctp_quarantine):
+    """Take all files from CTP quarantine folders and sort them by job"""
+
+    # IDIS quarantine should be empty to start
+    assert len(list(idis_ctp_quarantine.base_folder.glob("*"))) == 0
+    assert idis_ctp_quarantine.get_job_ids() == []
+
+    # move all files from CTP quarantine to IDIS quarantine and check
+    idis_ctp_quarantine.scrape()
+
+    assert idis_ctp_quarantine.get_job_ids() == [1, 2, 3]
+    assert len(idis_ctp_quarantine.get_files(job_id=1)) == 2
+    assert len(idis_ctp_quarantine.get_files(job_id=2)) == 2
+    assert len(idis_ctp_quarantine.get_files(job_id=3)) == 1
+    assert len(idis_ctp_quarantine.get_unknown_job_files()) == 3
+
+    # unknown jobs should not raise exceptions
+    assert idis_ctp_quarantine.get_files(job_id=100) == []
+
+
+def test_idis_ctp_archiving(idis_ctp_quarantine):
+    """You can archive all files for a job, moving them out of the way or regular processing, but not deleting"""
+    # make sure there are some files in IDIS quarantine
+    idis_ctp_quarantine.scrape()
+    assert idis_ctp_quarantine.get_job_ids() == [1, 2, 3]
+
+    # check how many files are in active quarantine for job 2
+    files_for_job = idis_ctp_quarantine.get_files(job_id=2)
+
+    # now archive the files for this job
+    idis_ctp_quarantine.archive(job_id=2)
+
+    # now this job should no longer appear
+    assert idis_ctp_quarantine.get_job_ids() == [1, 3]
+    # and have no files active
+    assert idis_ctp_quarantine.get_files(job_id=2) == []
+
+    # and the two files should have been moved to archive
+    assert len([x for x in idis_ctp_quarantine.archived_base_folder.rglob('*') if x.is_file()]) == len(files_for_job)
+
+

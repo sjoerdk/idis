c302225e2d2085a6104080b474d32fc26c77ec1e
Sjoerd Kerkstra <sjoerd.kerkstra@radboudumc.nl>
2020-03-12 11:58:55 +0100

    Bump version: 0.1.22 → 0.1.23

--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,7 @@ copyright = """2018, Sjoerd Kerkstra"""
 # built documents.
 #
 # The short X.Y version.
-version = "version = 'version = '0.1.22''"
+version = "version = 'version = '0.1.23''"
 # The full version, including alpha/beta/rc tags.
 release = version
 
